202495781babd015a45fd140413877216ffb9198
BasketBandit <errorsfound@hotmail.co.jp>
2020-10-13 16:05:39 +0100

    first piece of C code written

---
+++ oddeven.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int input = 0;
+
+int main() {
+    printf("Enter integer to find out if it is odd or even!\n");
+    scanf("%d", &input);
+    printf(input % 2 == 0 ? "Even" : "Odd");
+    return 0;
+}
