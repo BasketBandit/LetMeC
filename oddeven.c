35339b6343cf33a8507b2a0477df3dcbd9c876d3
BasketBandit <errorsfound@hotmail.co.jp>
2020-10-14 23:33:39 +0100

    add newline to oddeven.c

--- oddeven.c
+++ oddeven.c
@@ -5,6 +5,6 @@ int input = 0;
 int main() {
     printf("Enter integer to find out if it is odd or even!\n");
     scanf("%d", &input);
-    printf(input % 2 == 0 ? "Even" : "Odd");
+    printf(input % 2 == 0 ? "Even\n" : "Odd\n");
     return 0;
 }
