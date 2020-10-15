d9a628abba3f115e4235b113bbec106baa885d9f
BasketBandit <errorsfound@hotmail.co.jp>
2020-10-15 17:36:11 +0100

    learned about header files and how they're used with includes

--- src/math/oddeven.c
+++ src/math/oddeven.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int input = 0;
+void oddeven(); // function prototype
 
-int main() {
+void oddeven() {
+    int input = 0;
     printf("Enter integer to find out if it is odd or even!\n");
     scanf("%d", &input);
     printf(input % 2 == 0 ? "Even\n" : "Odd\n");
-    return 0;
 }
---
+++ src/math/oddeven.h
@@ -0,0 +1,6 @@
+#ifndef LETMEC_ODDEVEN_H
+#define LETMEC_ODDEVEN_H
+
+void oddeven();
+
+#endif //LETMEC_ODDEVEN_H
