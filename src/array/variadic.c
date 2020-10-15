a21c4b97bf71016c0d26296e9b018326e482b784
BasketBandit <errorsfound@hotmail.co.jp>
2020-10-15 17:36:31 +0100

    learned about how variable argument functions work in c

---
+++ src/array/variadic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void variadic(int nums, ...); // function prototype
+
+void variadic(int nums, ...) {
+    va_list list;
+    va_start(list, nums);
+
+    int total = 0;
+    for(int i = 0; i < nums; i++) {
+        total += va_arg(list, int);
+    }
+
+    va_end(list);
+
+    printf("%d", total);
+}
---
+++ src/array/variadic.h
@@ -0,0 +1,6 @@
+#ifndef LETMEC_VARIADIC_H
+#define LETMEC_VARIADIC_H
+
+void variadic(int nums, ...);
+
+#endif //LETMEC_VARIADIC_H
