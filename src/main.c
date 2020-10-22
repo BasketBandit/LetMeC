c1316848335003a0d4c930385b47ef3eaa96e071
BasketBandit <errorsfound@hotmail.co.jp>
2020-10-22 02:54:34 +0100

    added a good boy to be given away every time the program runs

--- src/main.c
+++ src/main.c
@@ -6,6 +6,10 @@
 #include "struct/dog.h"
 
 int main() {
+    Dog dog = new_Dog("Good Boy", 2, "Big", "Brown");
+    printf("%s, %d, %s, %s \n", Dog_getName(dog), Dog_getAge(dog), Dog_getType(dog), Dog_getColour(dog));
+    Dog_giveAway(dog); // clears memory;
+
     oddeven();
     variadic(10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     dereference();
@@ -13,7 +17,7 @@ int main() {
     newDogScan();
 
     int x;
-    scanf("%d", &x);
+    scanf("%d", &x); // stalls terminal so output is readable
 
     return 0;
 }
